f9c7f8f82fddbc939463d858da42f5e3224fb4a6
uilherm <guilhermepmundim@gmail.com>
2024-11-25 15:07:25 -0300

    Criando o grafo

--- src/main/java/iesb/pg2024/project/app/ProjectMain.java
+++ src/main/java/iesb/pg2024/project/app/ProjectMain.java
@@ -1,18 +1,75 @@
 package iesb.pg2024.project.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import iesb.pg2024.project.core.Grafo;
+
 //import iesb.pg2024.project.dataset.datasetGenerate;
 
 import iesb.pg2024.project.dataset.DatasetGenerator;
+import iesb.pg2024.project.search.BuscaEmProfundidade;
 
 public class ProjectMain {
-
+	
+		
 	public static void main(String[] args)
 		throws ReflectiveOperationException{
 		// TODO Auto-generated method stub
-		DatasetGenerator datasetGenerator = new DatasetGenerator();
-		datasetGenerator.generateDataSet();
+//		DatasetGenerator datasetGenerator = new DatasetGenerator();
+//		datasetGenerator.generateDataSet();
 		
-	}
+		Grafo grafo = null;
+		try {
+			File arestas = new File("C:\\Users\\Guilherme P. Mundim\\TG2024PROJECT\\src\\main\\java\\iesb\\pg2024\\project\\dataset\\arestas.txt");
+			BufferedReader br = new BufferedReader(new FileReader(arestas));
+			Set<String> vertices = new HashSet<String>();
+			String line;
+			String labels[] = new String[4];
+			
+			System.out.println(">> Counting nodes...");
+			do {
+				labels = br.readLine().split(";");
+				vertices.add(labels[0]);
+				vertices.add(labels[1]);
+			} while ((line = br.readLine())!= null);
+			System.out.println(">> Quantity of Nodes: [" + vertices.size() + "]");
+			
+			System.out.println(">> Adding Nodes...");
+			grafo = new Grafo(vertices.size());
+			for (String string : vertices) {
+				grafo.adicionarVertices(string);
+			}
+			System.out.println(">> Nodes added.");
+			br.close();
+			
+			System.out.println(">> Adding edges...");
+			br = new BufferedReader(new FileReader(arestas));
+			Pattern p = Pattern.compile("[0-9]+");
+			Matcher m;
+			do {
+				labels = br.readLine().split(";");
+				m = p.matcher(labels[3]);
+				if(m.matches()) {
+					grafo.conectarVertices(labels[0], labels[1], Integer.parseInt(labels[3]));
+//					System.out.println(">> " + labels[0] + " -> " + labels[1]);//test
+//					System.out.println(">> Weight: " + labels[3]);//test
+				}else {
+					grafo.conectarVertices(labels[0], labels[1], 0);
+				}
+			} while ((line = br.readLine())!= null);
+			br.close();
+			System.out.println(">> Edges added.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		}
 	/*String className = "java.lang.instrument";
             // Obtenha a classe pelo nome
             Class<?> clazz = Class.forName(className);
